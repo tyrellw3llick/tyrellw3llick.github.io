9ea8605815670d36b25c05105afdf6a25cfaa363
DeusExDev <enriquemartin1402@gmail.com>
2025-04-03 03:27:34 +0100

    feat: add katex support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 	site: 'https://tyrellw3llick.github.io',
 	vite: {
 		plugins: [tailwindcss()],
